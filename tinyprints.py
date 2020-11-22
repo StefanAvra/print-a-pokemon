d1f7341c7e434f3d655ff63c5e29ef08d5cda61c
stefanavra <stefan.avra@gmail.com>
2020-11-22 23:03:30 +0100

    test print

--- tinyprints.py
+++ tinyprints.py
@@ -1,9 +1,12 @@
 import serial
 import cups
+import os
 from datetime import datetime
 
 conn = cups.Connection()
 ser = serial.Serial('/dev/serial0')
+
+QUEUE_PATH = './queue/'
 
 
 def reset():
@@ -12,7 +15,13 @@ def reset():
 
 def print_png(filename):
     ser.write('{}\n{}\n'.format(datetime.now().isoformat(), filename).encode())
-    job_id = conn.printFile('ZJ-58', filename, '', {})
+    job_id = conn.printFile('ZJ-58', QUEUE_PATH + filename, '', {})
 
 
+def delete_from_queue(filename):
+    os.remove(QUEUE_PATH + filename)
+
+
+reset()
 print_png('test.png')
+delete_from_queue('test.png')
